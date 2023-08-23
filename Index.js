38d6fada3e54d1262378dcfb70b46ba3aafa5b38
SleepyMoru <87416908+NeMoru05@users.noreply.github.com>
2023-08-23 10:25:46 +0800

    Setting Loop

--- Index.js
+++ Index.js
@@ -1,13 +1,11 @@
-var map = L.map('map').setView([0, 0], 2);
+var map = L.map('map', {
+  minZoom: 2, // Adjust this value as needed
+  maxZoom: 5,  // Adjust this value as needed
+  maxBoundsViscosity: 1.0,
+  worldCopyJump: false, // Disable world wrapping
+}).setView([0, 0], 1);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
+  attribution: '&copy; OpenStreetMap contributors',
+  maxZoom: 19
 }).addTo(map);
-
-var imageUrl = 'test.jpg';
-var imageBounds = [[-500,0], [100, 500]];
-
-L.imageOverlay(imageUrl, imageBounds).addTo(map);
-
-var marker = L.marker([500, 500]).addTo(map);
-marker.bindPopup("Marker on the floor plan");
